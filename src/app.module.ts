1b302d284fa22093b5790f96dedef398e9d45cd4
benedict <kiokobenedict@gmail.com>
2020-04-03 18:23:22 +0300

    imported the dog module to the main app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
-import { CatModule } from './cat/cat.module';
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { DogModule } from './dog/dog.module';
+import { CatModule } from './cat/cat.module';
 
 @Module({
   imports: [
+        HttpModule,
+        DogModule, 
         CatModule, 
         MongooseModule.forRoot('mongodb://localhost/mongo-connect',
         {useNewUrlParser: true})
